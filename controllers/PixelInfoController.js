7b0dfc51b991990a4346e863e8530fbca44a223a
TennnyZhuang <zty0826@gmail.com>
2017-08-09 15:05:25 +0800

    fake api

--- controllers/PixelInfoController.js
+++ controllers/PixelInfoController.js
@@ -1,16 +1,45 @@
-const Pixel = require("../models/pixel")
+const Pixel = require('../models/pixel')
 
 exports.getAPIPixelInfo = (req, res, next) => {
     function fail(err) {
-        req.place.reportError("Pixel data retrieve error: " + err);
-        return res.status(500).json({ success: false, error: { message: "An error occurred while trying to look up information about that pixel." } })
+        req.place.reportError('Pixel data retrieve error: ' + err);
+        return res.status(500).json({
+            success: false,
+            error: {
+                message:
+            'An error occurred while trying to look up information about that pixel.'
+            }
+        });
     }
-    if(!req.query.x || !req.query.y) return res.status(400).json( { success: false, error: { message: "You did not specify the coordinates of the pixel to look up.", code: "bad_request" } });
+    if (!req.query.x || !req.query.y) {
+        return res.status(400).json({
+            success: false,
+            error: {
+                message: 'You did not specify the coordinates of the pixel to look up.',
+                code: 'bad_request'
+            }
+        });
+    }
     var overrideDataAccess = req.user && (req.user.moderator || req.user.admin);
-    Pixel.find({xPos: req.query.x, yPos: req.query.y}).then((pixels) => {
-        if (pixels.length <= 0) return res.json({ success: true, pixel: null });
-        pixels[0].getInfo(overrideDataAccess, req.place).then((info) => {
-            res.json({ success: true, pixel: info })
-        }).catch((err) => fail(err));
-    }).catch((err) => fail(err));
+    Pixel.find({xPos: req.query.x, yPos: req.query.y})
+        .then((pixels) => {
+            if (pixels.length <= 0) return res.json({success: true, pixel: null});
+            pixels[0]
+                .getInfo(overrideDataAccess, req.place)
+                .then((info) => {res.json({
+                    success: true, 
+                    pixel: info,
+                    things: [
+                        {
+                            img_url: 'https://i.loli.net/2017/08/09/598a9011c1f3c.png',
+                            img_desc: 'microsoft',
+                            img_pos: [20, 20, 50, 50],
+                            img_detail_url: 'https://www.microsoft.com/',
+                            confidence: 0.8523550916,
+                        }
+                    ]
+                })})
+                .catch((err) => fail(err));
+        })
+        .catch((err) => fail(err));
 };
